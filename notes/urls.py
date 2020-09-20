31ea5e0e1363779dc995132d56e7aef698a4f2fa
ankeetkarki <ankeetkarki30@gmail.com>
2020-09-20 10:46:50 +0545

    Fetched todo from database

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
 	path('todos/', views.todopage, name = 'todo-page'),
 	path('addtodo/', views.addtodo, name = 'add-page')
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDo
 
 
 # Create your views here.
 def todopage(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'datas':ToDo.objects.all()})
 
 def addtodo(request):
 	return render(request, 'add.html')
