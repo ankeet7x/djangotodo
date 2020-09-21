d9b364333a734079284d893d29f62161d1438754
ankeetkarki <ankeetkarki30@gmail.com>
2020-09-21 20:28:17 +0545

    Few changes

--- notes/views.py
+++ notes/views.py
@@ -5,6 +5,7 @@ import json
 # from rest_framework import serializers
 # from django.core import serializers
 from .forms import ToDoForm
+from django.views.decorators.http import require_POST
 
 
 # Create your views here.
@@ -14,6 +15,16 @@ def todopage(request):
 	# datas = json.loads(json_data)
 	return render(request, 'home.html', {'datas': datas})
 
+
+
 def addtodo(request):
-	form = ToDoForm()
+	if request.method == 'POST':
+		form = ToDoForm(title = title, description = desc)
+		title = request.POST.get("title")
+		desc = request.POST.get("description")
+		# form = ToDoForm(request.POST)
+		
+		form.save()
+		
 	return render(request, 'add.html', {'form': form})
+
